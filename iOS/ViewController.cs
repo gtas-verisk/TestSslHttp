add1c9561fe0c2c913348e9e839a5b2b7450e45e
gtas-verisk <gtaskos@verisk.com>
2016-09-02 22:39:51 -0400

    Update ViewController.cs
    Commented code with Refit.
    Added HttpClient code to retrieve data from the endpoint directly. Problem 500 persists.

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using UIKit;
 
 namespace TestHttpSsl.iOS
@@ -31,7 +32,22 @@ namespace TestHttpSsl.iOS
 			{
 				try
 				{
-					string result = await ApiService.WebApi.GetInitialOrUpdatedData("f6f83e7d-8c8c-4a82-99b5-677d1a2ec608", 60136).ConfigureAwait(true);
+					//string result = await ApiService.WebApi.GetInitialOrUpdatedData("f6f83e7d-8c8c-4a82-99b5-677d1a2ec608", 60136).ConfigureAwait(true);
+
+					HttpClient hc = new HttpClient(new NSUrlSessionHandler());
+
+					string url = "https://flightkitapi.iso.com/sync/Order?workerid=43303&clientid=" + Guid.NewGuid().ToString();
+
+					hc.BaseAddress = new Uri(url);
+
+					var x = hc.BaseAddress.PathAndQuery;
+
+					hc.DefaultRequestHeaders.Accept.Clear();
+					hc.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					hc.DefaultRequestHeaders.Add("access_token", "a0ee216f-621d-4065-a171-bf49c51af636");
+
+					HttpResponseMessage result = await hc.GetAsync(url);
+
 					Debug.WriteLine("JSON Result: {0}", result);
 				}
 				catch (Exception ex)
